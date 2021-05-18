d8c5cd35d57b39b6eac4e349bac30d14c8cd7509
Mindaugas Kapickas <mindaugas@mkp.com>
2021-05-18 15:33:57 +0300

    3 uzd done camelCase

---
+++ js/functions.js
@@ -0,0 +1,10 @@
+"use strict";
+// 3. functions.js faile aprasyti funkcija, kuri gauna texta arba stringa ir pavercia ji camelCase (istrint tarpa ir kita raide in capital letters)
+
+function makeCamel(str) {
+  return str.replace(/(?:^\w|[A-Z]|\b\w|\s+)/g, function (match, index) {
+    if (+match === 0) return ""; // or if (/\s+/.test(match)) for white spaces
+    return index === 0 ? match.toLowerCase() : match.toUpperCase();
+  });
+}
+console.log(makeCamel("pasikurti nauja projekta inicializuoti git"));
